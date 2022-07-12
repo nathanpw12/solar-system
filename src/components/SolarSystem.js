91bc0866909462370ff72e891592d549e61cf380
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-12 19:53:45 -0300

    Requisito 6: Aprovado

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,6 +9,14 @@ class SolarSystem extends React.Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        <PlanetCard planetName={ planets[0].name } planetImage={ planets[0].image } />
+        <PlanetCard planetName={ planets[1].name } planetImage={ planets[1].image } />
+        <PlanetCard planetName={ planets[2].name } planetImage={ planets[2].image } />
+        <PlanetCard planetName={ planets[3].name } planetImage={ planets[3].image } />
+        <PlanetCard planetName={ planets[4].name } planetImage={ planets[4].image } />
+        <PlanetCard planetName={ planets[5].name } planetImage={ planets[5].image } />
+        <PlanetCard planetName={ planets[6].name } planetImage={ planets[6].image } />
+        <PlanetCard planetName={ planets[7].name } planetImage={ planets[7].image } />
       </>
     );
   }
