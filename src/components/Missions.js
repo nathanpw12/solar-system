7938c2f205531f44a85cff1c570324f98c4590e7
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-12 20:01:25 -0300

    Requisito 8: Aprovado

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Title from './Title';
 // import PropTypes from 'prop-types';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
