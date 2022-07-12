5a3ef5162a553463fd593fac934a8ecfac94d928
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-12 19:59:23 -0300

    Requisito 7: Aprovado

---
+++ src/components/Missions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+// Missions.propTypes = {
+//   planetName: PropTypes.string.isRequired,
+//   planetImage: PropTypes.string.isRequired,
+// };
+
+export default Missions;
